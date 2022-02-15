9d5c6c6a3f07bf075fac3af34ad9f6436e1afb61
Wilson <s31302@gmail.com>
2022-02-15 13:58:05 -0800

    add poetry enviroment and file testing scripts

--- test.py
+++ test.py
@@ -1,25 +1,29 @@
 from pathlib import Path
+import os
+from unittest import result
 
-# with open("testFile.txt", "r+", encoding = "utf-8") as file:
+file = open("results_22 - Done/CuratedList.tsv", "r+")
+# file.write("123\n")
 
-#     # Move the pointer (similar to a cursor in a text editor) to the end of the file
-#     file.seek(0, os.SEEK_END)
+file.seek(0, os.SEEK_END)
 
-#     # This code means the following code skips the very last character in the file -
-#     # i.e. in the case the last line is null we delete the last line
-#     # and the penultimate one
-#     pos = file.tell() - 1
-#     print(pos)
-#     # Read each character in the file one at a time from the penultimate
-#     # character going backwards, searching for a newline character
-#     # If we find a new line, exit the search
-#     while pos > 0 and file.read(1) != "\n":
-#         pos -= 1
-#         file.seek(pos, os.SEEK_SET)
+        # This code means the following code skips the very last character in the file -
+        # i.e. in the case the last line is null we delete the last line
+        # and the penultimate one
+pos = file.tell() - 3
+file.seek(pos)
+# Read each character in the file one at a time from the penultimate
+# character going backwards, searching for a newline character
+# If we find a new line, exit the search
+while pos > 0 and file.read(1) != "\n":
+    pos -= 1
+    print(pos)
+    file.seek(pos, os.SEEK_SET)
 
-#     # So long as we're not at the start of the file, delete all the characters ahead
-#     # of this position
-#     if pos > 0:
-#         file.seek(pos, os.SEEK_SET)
-#         file.truncate()
-print(Path(__file__).parent)
+# So long as we're not at the start of the file, delete all the characters ahead
+# of this position
+# if pos > 0:
+#     file.seek(pos, os.SEEK_SET)
+line = file.readline()
+file.seek(pos)
+file.close()
